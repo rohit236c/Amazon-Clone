350baaef58cda69a208feef9539a12221c78ce4b
rohit236c <rohit236c@gmail.com>
2020-09-16 13:47:02 +0530

    product added
    Signed-off-by: rohit236c <rohit236c@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Prime from './images/img_two.jpg';
 import './Home.css';
+import Product from './Product';
 function Home() {
     return (
         <div className="home">
@@ -8,10 +9,8 @@ function Home() {
             <img
                 className="home__img"
                 alt=""
-                src="https://images-eu.ssl-images-amazon.com/images/G/02/
-digital/video/merch2016/Hero/Covid19/Generic/
-GWBleedingHero_ENG_COVIDUPDATE__
-XSite_1500x600_PV_en-GB._CB428684220_.jpg"/>
+                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"/>
+            <Product title="hello world" price="500" rating={5} image = "https://assets.entrepreneur.com/content/3x2/2000/20191219170611-GettyImages-1152794789.jpeg?width=700&crop=2:1"/>
         </div>
     )
 };
---
+++ src/Product.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Product.css'
+function Product({title, id, rating, image, price}) {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>Rs</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_) => (
+                            <p>*</p>
+                        ))}
+                </div>
+            </div>
+
+            <img src={image} alt=""/>
+            <button>Add to Basket</button>
+        </div>
+    )
+};
+
+export default Product;
